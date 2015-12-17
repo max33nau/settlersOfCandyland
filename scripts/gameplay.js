bbf9b7f3243bd29a81794b83308637ea742460fc
max33nau <mtj29@nau.edu>
2015-12-17 09:33:15 -0800

    click tests

--- scripts/gameplay.js
+++ scripts/gameplay.js
@@ -2,16 +2,17 @@
 $(function() {
 
 $building = $('.building');
+$road = $('.road');
 
 $building.click(function() {
   $specificBuilding = $(this);
   $specificBuilding.css('background-color','red');
 });
 
-
-
-
-
+$road.click(function() {
+  $specificRoad = $(this);
+  $specificRoad.css('background-color','red');
+});
 
 
 })
