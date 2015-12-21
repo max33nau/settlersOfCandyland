6fccdf26dfe3722a8f99cb972f989cdd8e334a92
max33nau <mtj29@nau.edu>
2015-12-20 18:41:03 -0800

    ice cream sundaes working except for one

--- script/building.js
+++ script/building.js
@@ -1,40 +1,72 @@
 var building = {};
 
 building.buildUpperBuilding = function($tile) {
-  $tile.append('<div class="upperBuilding "></div>');
+  $tile.append('<div class="upperBuilding cupcake"></div>');
   $tile.find('.upperBuilding').css('background-color',playerInfo[my.currentPlayer].color);
   playerInfo[my.currentPlayer].numberOfCupcakes--;
 }
 
 building.buildUpperLeftBuilding = function($tile) {
-  $tile.append('<div class="topLeftBuilding"></div>');
+  $tile.append('<div class="topLeftBuilding cupcake"></div>');
   $tile.find('.topLeftBuilding').css('background-color',playerInfo[my.currentPlayer].color);
   playerInfo[my.currentPlayer].numberOfCupcakes--;
 }
 
 building.buildUpperRightBuilding = function($tile) {
-  $tile.append('<div class="topRightBuilding"></div>');
+  $tile.append('<div class="topRightBuilding cupcake"></div>');
   $tile.find('.topRightBuilding').css('background-color',playerInfo[my.currentPlayer].color);
   playerInfo[my.currentPlayer].numberOfCupcakes--;
 }
 
 building.buildBottomBuilding = function($tile) {
-  $tile.append('<div class="bottomBuilding"></div>');
+  $tile.append('<div class="bottomBuilding cupcake"></div>');
   $tile.find('.bottomBuilding').css('background-color',playerInfo[my.currentPlayer].color);
   playerInfo[my.currentPlayer].numberOfCupcakes--;
 }
 
 building.buildBottomLeftBuilding = function($tile) {
-  $tile.append('<div class="bottomLeftBuilding"></div>');
+  $tile.append('<div class="bottomLeftBuilding cupcake"></div>');
   $tile.find('.bottomLeftBuilding').css('background-color',playerInfo[my.currentPlayer].color);
   playerInfo[my.currentPlayer].numberOfCupcakes--;
 }
 
 building.buildBottomRightBuilding = function($tile) {
-  $tile.append('<div class="bottomRightBuilding"></div>');
+  $tile.append('<div class="bottomRightBuilding cupcake"></div>');
   $tile.find('.bottomRightBuilding').css('background-color',playerInfo[my.currentPlayer].color);
   playerInfo[my.currentPlayer].numberOfCupcakes--;
 }
+
+building.buildBottomRightIceCreamSundae = function($tile) {
+  $tile.find('.bottomRightBuilding').removeClass('cupcake').addClass('iceCreamSundae');
+  playerInfo[my.currentPlayer].numberOfIceCreamSundaes--;
+}
+
+building.buildBottomLeftIceCreamSundae = function($tile) {
+  $tile.find('.bottomLeftBuilding').removeClass('cupcake').addClass('iceCreamSundae');
+  playerInfo[my.currentPlayer].numberOfIceCreamSundaes--;
+}
+
+building.buildBottomIceCreamSundae = function($tile) {
+  $tile.find('.bottomBuilding').removeClass('cupcake').addClass('iceCreamSundae');
+  playerInfo[my.currentPlayer].numberOfIceCreamSundaes--;
+}
+
+building.buildUpperIceCreamSundae = function($tile) {
+  $tile.find('.upperBuilding').removeClass('cupcake').addClass('iceCreamSundae');
+  playerInfo[my.currentPlayer].numberOfIceCreamSundaes--;
+}
+
+building.buildTopRightIceCreamSundae = function($tile) {
+  $tile.find('.topRightBuilding').removeClass('cupcake').addClass('iceCreamSundae');
+  playerInfo[my.currentPlayer].numberOfIceCreamSundaes--;
+}
+
+building.buildTopLeftIceCreamSundae = function($tile) {
+  $tile.find('.topLeftBuilding').removeClass('cupcake').addClass('iceCreamSundae');
+  playerInfo[my.currentPlayer].numberOfIceCreamSundaes--;
+}
+
+
 
 building.upper = function($specificTile, $previousTile, $nextTile, $upperLeftTile, $upperRightTile, $bottomLeftTile, $bottomRightTile,$straightAboveTile, playerColor) {
   // CHECKS TOP BUILDING SPOT
@@ -42,11 +74,16 @@ building.upper = function($specificTile, $previousTile, $nextTile, $upperLeftTil
   $tileTopLeftBottomRightBuilding = $upperLeftTile.find('.bottomRightBuilding');
   $tileTopRightBottomLeftBuilding = $upperRightTile.find('.bottomLeftBuilding');
 
-  if ($tileTopBuilding.length > 0 || $tileTopLeftBottomRightBuilding.length > 0 || $tileTopRightBottomLeftBuilding.length > 0 ) {
-    console.log('build ice cream sundae');
+  if ($tileTopBuilding.length > 0) {
+    building.buildUpperIceCreamSundae($specificTile);
+    return;
+  } else if($tileTopLeftBottomRightBuilding.length > 0) {
+    building.buildBottomRightIceCreamSundae($upperLeftTile);
+    return;
+  } else if ($tileTopRightBottomLeftBuilding.length > 0) {
+    building.buildBottomLeftIceCreamSundae($upperRightTile);
     return;
   } else {
-    console.log('here');
     $tileUpperLeftUpperRightBuilding = $upperLeftTile.find('.topRightBuilding');
     $tileUpperRightUpperLeftBuilding = $upperRightTile.find('.topLeftBuilding');
     $tileAboveUpperLeftRightBottomBuilding = $straightAboveTile.find('.bottomBuilding');
@@ -81,8 +118,14 @@ building.topLeft = function($specificTile, $previousTile, $nextTile, $upperLeftT
   $tileTopLeftBuilding = $specificTile.find('.topLeftBuilding');
   $tilePreviousTopRightBuilding = $previousTile.find('.topRightBuilding');
 
-  if ($tileUpperLeftBottomBuilding.length > 0 || $tileTopLeftBuilding.length > 0 ||   $tilePreviousTopRightBuilding.length > 0 ) {
-    console.log('build ice cream sundae');
+  if ($tileTopLeftBuilding.length > 0) {
+    building.buildTopLeftIceCreamSundae($specificTile);
+    return;
+  } else if ($tileUpperLeftBottomBuilding.length > 0) {
+    building.buildBottomIceCreamSundae($upperLeftTile);
+    return;
+  } else if($tilePreviousTopRightBuilding.length > 0) {
+    building.buildTopRightIceCreamSundae($previousTile);
     return;
   } else {
 
@@ -122,8 +165,14 @@ building.topRight = function($specificTile, $previousTile, $nextTile, $upperLeft
   $tileNextTopLeftBuilding = $nextTile.find('.topLeftBuilding');
   $tileUpperRightBottomBuilding = $upperRightTile.find('.bottomBuilding');
 
-  if ($tileTopRightBuilding.length > 0 || $tileNextTopLeftBuilding.length > 0 ||   $tileUpperRightBottomBuilding.length > 0 ) {
-    console.log('build ice cream sundae');
+  if ($tileTopRightBuilding.length > 0) {
+    building.buildTopRightIceCreamSundae($specificTile);
+    return;
+  } else if($tileNextTopLeftBuilding.length > 0) {
+    building.buildTopLeftIceCreamSundae($nextTile);
+    return;
+  } else if($tileUpperRightBottomBuilding.length > 0) {
+    building.buildBottomIceCreamSundae($upperRightTile);
     return;
   } else {
 
@@ -161,10 +210,16 @@ building.bottom = function($specificTile, $previousTile, $nextTile, $upperLeftTi
   // CHECKS BOTTOM BUILDING SPOT
   $tileBottomBuilding = $specificTile.find('.bottomBuilding');
   $tileBottomLeftUpperRightBuilding = $bottomLeftTile.find('.topRightBuilding');
-  $tileBottomRightUpperLeftBuilding = $upperRightTile.find('.topLeftBuilding');
+  $tileBottomRightUpperLeftBuilding = $bottomRightTile.find('.topLeftBuilding');
 
-  if ($tileBottomBuilding.length > 0 || $tileBottomLeftUpperRightBuilding.length > 0 ||   $tileBottomRightUpperLeftBuilding.length > 0 ) {
-    console.log('build ice cream sundae');
+  if ($tileBottomBuilding.length > 0 ) {
+    building.buildBottomIceCreamSundae($specificTile);
+    return;
+  } else if ($tileBottomLeftUpperRightBuilding.length > 0) {
+    building.buildTopRightIceCreamSundae($bottomLeftTile);
+    return;
+  } else if ($tileBottomRightUpperLeftBuilding.length > 0) {
+    building.buildTopLeftIceCreamSundae($bottomRightTile);
     return;
   } else {
 
@@ -204,8 +259,14 @@ building.bottomLeft = function($specificTile, $previousTile, $nextTile, $upperLe
   $tileBottomLeftBuilding = $specificTile.find('.bottomLeftBuilding');
   $tileBottomLeftUpperBuilding = $bottomLeftTile.find('.upperBuilding');
 
-  if ($tilePreviousBottomRightBuilding.length > 0 || $tileBottomLeftBuilding.length > 0 ||   $tileBottomLeftUpperBuilding.length > 0 ) {
-    console.log('build ice cream sundae');
+  if ($tilePreviousBottomRightBuilding.length > 0) {
+    building.buildBottomRightIceCreamSundae($previousTile);
+    return;
+  } else if ($tileBottomLeftBuilding.length > 0) {
+    building.buildBottomLeftIceCreamSundae($specificTile);
+    return;
+  } else if ($tileBottomLeftUpperBuilding.length > 0) {
+    building.buildUpperIceCreamSundae($bottomLeftTile);
     return;
   } else {
 
@@ -217,7 +278,7 @@ building.bottomLeft = function($specificTile, $previousTile, $nextTile, $upperLe
     // CHECKS BOTTOM BUILDING SPOT
     $tileBottomBuilding = $specificTile.find('.bottomBuilding');
     $tileBottomLeftUpperRightBuilding = $bottomLeftTile.find('.topRightBuilding');
-    $tileBottomRightUpperLeftBuilding = $upperRightTile.find('.topLeftBuilding');
+    $tileBottomRightUpperLeftBuilding = $bottomRightTile.find('.topLeftBuilding');
 
     $tilePreviousBottomBuilding = $previousTile.find('.bottomBuilding');
     $tileBottomLeftUpperLeftBuilding = $bottomLeftTile.find('.topLeftBuilding');
@@ -246,7 +307,7 @@ building.bottomRight = function($specificTile, $previousTile, $nextTile, $upperL
   $tileBottomRightUpperBuilding = $bottomRightTile.find('.upperBuilding');
 
   if ($tileBottomRightBuilding.length > 0 || $tileNextBottomLeftBuilding.length > 0 ||   $tileBottomRightUpperBuilding.length > 0 ) {
-    console.log('build ice cream sundae');
+    building.buildBottomRightIceCreamSundae($specificTile);
     return;
   } else {
 
@@ -258,7 +319,7 @@ building.bottomRight = function($specificTile, $previousTile, $nextTile, $upperL
     // CHECKS BOTTOM BUILDING SPOT
     $tileBottomBuilding = $specificTile.find('.bottomBuilding');
     $tileBottomLeftUpperRightBuilding = $bottomLeftTile.find('.topRightBuilding');
-    $tileBottomRightUpperLeftBuilding = $upperRightTile.find('.topLeftBuilding');
+    $tileBottomRightUpperLeftBuilding = $bottomRightTile.find('.topLeftBuilding');
 
     $tileNextBottomBuilding = $nextTile.find('.bottomBuilding');
     $tileBottomRightUpperRightBuilding = $bottomRightTile.find('.topRightBuilding');
