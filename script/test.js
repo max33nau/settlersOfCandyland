2035c5deec422e55d41e46de8cfe05c6f95d8192
max33nau <mtj29@nau.edu>
2015-12-22 12:38:57 -0800

    fixed check boxes

--- script/test.js
+++ script/test.js
@@ -56,18 +56,22 @@ if(my.currentGame) {
   my.currentPlayerFieldset.append('<img class="currentPlayerImage" src='+playerInfo[my.currentPlayer].imageUrl+'>');
 }
 
+
+
 var $tiles = $('.check');
 // Checks if on a touch screen device
 if (hasTouch) {
-  $('.churroLocation').on('touchstart', function() {
+  $('.churroLocation').on('touchend', function() {
     $thisLocation = $(this);
     my.churroLocation = $thisLocation.attr('id');
   });
-  $('.dessertLocation').on('touchstart', function() {
+  $('.dessertLocation').on('touchend', function() {
     $thisLocation = $(this);
     my.dessertLocation = $thisLocation.attr('id');
   });
-  $tiles.on('touchstart',checkButton);
+  $tiles.on('touchend',checkButton);
+  $('#rollDiceButton').on('touchend',diceRolled);
+
 } else {
   $('.churroLocation').on('click', function() {
     $thisLocation = $(this);
@@ -78,8 +82,29 @@ if (hasTouch) {
     my.dessertLocation = $thisLocation.attr('id');
   });
   $tiles.on('click',checkButton);
+
+  $('#rollDiceButton').on('click',diceRolled);
 }
 
+function diceRolled() {
+  var $die1 = $('#die1');
+  var $die2 = $('#die2');
+  var $displayTotal = $('#displayTotal');
+  my.currentPlayerFieldset.empty();
+  console.log('button clicked');
+  var roll1 = Math.floor(Math.random() * 6) +1;
+  var roll2 = Math.floor(Math.random() * 6) +1;
+  var total = roll1 + roll2;
+  $die1.text(roll1);
+  $die2.text(roll2);
+  $displayTotal.text(total);
+  if(my.currentPlayer == my.totalPlayers) {
+    my.currentPlayer = -1;
+   }
+  my.currentPlayer++;
+  my.currentPlayerFieldset.css('background-color',playerInfo[my.currentPlayer].color);
+  my.currentPlayerFieldset.append('<img class="currentPlayerImage" src='+playerInfo[my.currentPlayer].imageUrl+'>');
+}
 function checkButton() {
   my.thiefButton=$('input[type="radio"][name="thiefLocation"]:checked').val();
   $tile = $(this).parent();
