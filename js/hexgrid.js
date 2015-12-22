b53615d760dd8b504abd1c2914699c003ff361d4
John Hearn <jhearn@gmail.com>
2015-12-21 20:33:01 -0800

    I think neighbor relations are set correctly during board construction (currently only for radius 2)

--- js/hexgrid.js
+++ js/hexgrid.js
@@ -7,12 +7,14 @@
 
 var oddHours =  [ 1, 3, 5, 7, 9, 11 ];
 var evenHours = [ 0, 2, 4, 6, 8, 10 ];
-var allHours = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 ];
+var allHours =  [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 ];
+var tileIndex = new TileIndex();
 
 // Note for directions: odd hours are adjacent tiles,
-// but even hours are actually "two" away.
+// but even hours are actually "two" steps away.
+// This comes in handy, eventually.
 var direction = [ new Coordinate(1,-2),
-		  new Coordinate(1,1),
+		  new Coordinate(1,-1),
 		  new Coordinate(2,-1),
 		  new Coordinate(1,0),
 		  new Coordinate(1,1),
@@ -29,9 +31,42 @@ var isNull = function( x ) {
 }
 
 var invert = function( hour ) {
-    return  ( hour + 6 )  % 12;
+    return  ( Number(hour) + 6 )  % 12;
 } 
 
+function TileIndex() {
+    var my = [ ];
+    
+    my.findTileByCoord = function(coord) {
+	var foundTile = my.filter( function(tile) {
+	    return ( coord.x === tile.coord.x && coord.z === tile.coord.z );
+	});
+	
+	if ( foundTile.length === 0 ) {
+	    //console.log("Tile at (" + coord.x + ", " + coord.z + ") not found.");
+	    return null;
+	}
+	else if ( foundTile.length === 1 ) {
+	    //console.log("Tile found at (" + coord.x + ", " + coord.z + ")");
+	    return foundTile[0];
+	} else {
+	    console.log("ERROR: More than one tile found at (" + coord.x + ", " + coord.z + ")");
+	    return null;
+	}
+    };
+
+    my.logTiles = function(){
+	var output =
+	    "TileIndex: " +
+	    my.map(function(tile){
+		return "(" + tile.coord.x + ", " + tile.coord.z + ")";
+	    })
+	    .join(", ");
+	console.log(output);
+    };
+    return my;
+}
+    
 function Neighbor() {
     var self = this;
     self.edge = null;
@@ -65,6 +100,7 @@ function Coordinate( q, r ) {
     self.move = function(direction) {
 	var q = self.x + direction.x;
 	var r = self.z + direction.z;
+	//console.log( "q: " + q + ", r: " + r );
 	return new Coordinate(q, r);
     }
 
@@ -80,20 +116,64 @@ function Tile( q, r ) {
     self.type = null;
     self.findMyself = findMyself;
 
-    self.setNeighbor = function(tile){
-	self.neighbors[ self.findMyself(tile) ] = tile;
+    self.setNeighbor = function(tile) {
+	var hour = self.findMyself(tile);
+	self.neighbors[ hour ].tile = tile;
     }
 
-    // Put existing element detection code here.
+    self.logNeighbors = function() {
+	var output =  "At (" + self.coord.x + ", "
+	                  + self.coord.z + "): \n" +
+	    self.neighbors
+	    .map( function(neighbor, hour){
+		if( Number(hour) % 2 == 0 ) {
+		    return "";
+		} else { return hour + ": ("
+		       + neighbor.tile.coord.x + ", "
+		       + neighbor.tile.coord.z + ")   "; } })
+	    .join("");
+	console.log(output);
+    };
+
     
-    oddHours.forEach(function(hour){
+    // Put existing neighbor detection code here.
+    oddHours.forEach( function(hour) {
+	// Check to see if we've already set neighbor relationships for tiles
+	// at adjacent coords. If not, and neighboring tiles exist in the index,
+	// set neighboring tile, edge, and vertex relationships.
+	if( self.neighbors[hour].tile === null) {
+	    // If null, then the tile key exists, but no relationship has been set
+	    
+	    var tile = tileIndex.findTileByCoord( self.coord.move(direction[hour]) );
+
+	    // We may be redundantly setting vertex and edge relationships,
+	    // but it's okay
+	    if( tile != null ) {
+		self.neighbors[hour].tile = tile;
+		self.neighbors[hour].edge = tile.neighbors[invert(hour)].edge;
+		self.neighbors[ (hour + 1) % 12 ].edge =
+		    tile.neighbors[ (invert(hour) + 11) % 12 ].edge;
+		self.neighbors[ (hour + 1) % 12].vertex =
+		    tile.neighbors[ (invert(hour) + 11) % 12].vertex;
+		self.neighbors[ (hour + 11) % 12 ].edge =
+		    tile.neighbors[ (invert(hour) + 1) % 12 ].edge;
+		self.neighbors[ (hour + 11) % 12].vertex =
+		    tile.neighbors[ (invert(hour) + 1) % 12].vertex;
+		tile.setNeighbor(self);
+	    }
+	}
+    });
+
+    
+    
+    // Add remaining adjacent vertices and edges to tile.
+    oddHours.forEach( function(hour){
 	if( self.neighbors[hour].isNull("edge") ) {
 	    self.neighbors[hour].edge = new Edge();
 	    self.neighbors[hour].edge.setNeighbor(self);
 	}
     });
-
-    evenHours.forEach(function(hour){
+    evenHours.forEach( function(hour){
 	if( self.neighbors[hour].isNull("vertex") ) {
 	    self.neighbors[hour].vertex = new Vertex();
 	    self.neighbors[hour].vertex.setNeighbor(self);
@@ -111,7 +191,9 @@ function Edge() {
     self.road = null;
 
     self.setNeighbor = function(tile) {
-	self.neighbors[ self.findMyself(tile) ] = tile;
+	var hour = self.findMyself(tile);
+	//console.log("ed");
+	self.neighbors[ hour ].edge = tile.neighbors[invert(hour)].edge;
     }
 }
 
@@ -124,7 +206,9 @@ function Vertex() {
     self.building = { type: null,
 		      color: null };
     self.setNeighbor = function(tile) {
-	self.neighbors[ self.findMyself(tile) ] = tile;
+	var hour = self.findMyself(tile);
+	//console.log("vert");
+	self.neighbors[ hour ].vertex = tile.neighbors[invert(hour)].vertex;
     }
 }
 
@@ -142,44 +226,36 @@ function HexBoard(radius) {
     var self = this;
     self.radius = radius;
     self.center = new Tile( 0, 0 );
-    self.tileIndex = [];
-    self.tileIndex.push(self.center);
-    self.findTileByCoord = function(q, r) {
-	return self.tileIndex
-	         .filter(function(tile) { return q === tile.coord.x
-					  && r === tile.coord.z; } );
-    }
+    tileIndex.push(self.center);
+
     
     self.init = function(){
-	// is this good indexing?
 	// see: http://www.redblobgames.com/grids/hexagons/#rings
-	//for ( var i = 0 < radius ) {  } //later
+	var currentTile = self.center;  //temp
+	for ( var i = 1; i < 2; i++ ) {
+	    oddHours.forEach(function(hour) {
 
-	// Brute force: this sucks, needs fix
-	// Make first ring
-	self.center.neighbors[1].tile = new Tile( 1, -1 );
-	self.center.neighbors[1].tile.setNeighbor(self.center);
-	self.center.neighbors[3].tile = new Tile( 1, 0 );
-	self.center.neighbors[3].tile.setNeighbor(self.center);
-	self.center.neighbors[5].tile = new Tile( 0, 1 );
-	self.center.neighbors[5].tile.setNeighbor(self.center);
-	self.center.neighbors[7].tile = new Tile( -1, 1 );
-	self.center.neighbors[7].tile.setNeighbor(self.center);
-	self.center.neighbors[9].tile = new Tile( -1, 0 );
-	self.center.neighbors[9].tile.setNeighbor(self.center);
-	self.center.neighbors[11].tile = new Tile( 0, -1 );
-	self.center.neighbors[11].tile.setNeighbor(self.center);
+		//console.log( tileIndex.findTileByCoord( currentTile.coord.move(direction[hour]) ) );
+		if ( tileIndex.findTileByCoord( currentTile.coord.move(direction[hour]) ) == null ) {
+		    var currentCoord = currentTile.coord.move(direction[hour]);
+		    currentTile.neighbors[hour].tile =
+			new Tile(currentCoord.x, currentCoord.z);
+		    tileIndex.push(currentTile.neighbors[hour].tile);
+		}
+	    });
+	}
 
-	console.log(self);
+	self.center.logNeighbors();
+	tileIndex.logTiles();
     }
 }
 
 var findMyself  = function( their ) {
     var self = this;
-    
-    for ( var angle in their.neighbors ) {
-	if ( self === their.neighbors[angle][self.whatAmI] ) {
-	    return invert(angle);
+    for ( var hour in their.neighbors ) {
+	if ( self === their.neighbors[hour][self.whatAmI] ) {
+	    //console.log(hour + " -> " + invert(hour));
+	    return invert(hour);
 	}
     }
     console.log( "ERROR: ", self, " couldn't find itself in ", their.neighbors );
@@ -189,7 +265,7 @@ var findMyself  = function( their ) {
 
 var HEX_BOARD_MODULE = (function() {
     var my = {};
-    var radius = 3;
+    var radius = 2;
     
     my.board = new HexBoard( radius );
     my.board.init();
