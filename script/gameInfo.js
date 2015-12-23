c466c5433ebd98012d7184c8dc5c8a24636b078f
max33nau <mtj29@nau.edu>
2015-12-23 10:50:56 -0800

    adds player details

--- script/gameInfo.js
+++ script/gameInfo.js
@@ -19,12 +19,12 @@ gameInfo.player3name = '';
 gameInfo.player4name = '';
 gameInfo.totalPlayers = 0;
 
-$('#startNewGame').click(function() {
+function resetGame() {
   localStorage.clear();
   location.reload();
-});
+}
 
-$('.playerNumber').click(function() {
+function checkNumberOfPlayers() {
   $thisNumber = $(this);
   if($thisNumber.text() =='3') {
     $('#player4name').hide();
@@ -36,43 +36,63 @@ $('.playerNumber').click(function() {
     console.log('something is wrong');
   }
   gameInfo.totalPlayers = Number($thisNumber.text());
-});
-
-$('#submit').click(function() {
-gameInfo.player1name = $('#player1name').val();
-gameInfo.player2name = $('#player2name').val();
-gameInfo.player3name = $('#player3name').val();
-gameInfo.player4name = $('#player4name').val();
-
-if(gameInfo.totalPlayers == 3) {
-  if(gameInfo.player1name && gameInfo.player2name && gameInfo.player3name) {
-    var player1 = new PlayerInfo('King Kandy', 'rgb(250, 161, 17)','graphics/KingKandy.png','cupCakesAndSundaes/cupCakeOrange.svg','cupCakesAndSundaes/sundeaOrange.svg',gameInfo.player1name);
-    var player2 = new PlayerInfo('Lord Licorice', 'rgb(202, 55, 55)','graphics/lordLicorice.png','cupCakesAndSundaes/cupCakeRed.svg','cupCakesAndSundaes/sundeaRed.svg',gameInfo.player2name);
-    var player3 = new PlayerInfo('Queen Frostine', 'rgb(102, 173, 229)','graphics/queenFrostine.png','cupCakesAndSundaes/cupCakeBlue.svg','cupCakesAndSundaes/sundeaBlue.svg',gameInfo.player3name);
-    playerInfo = [player1,player2,player3];
-    localStorage.setItem('currentPlayerInfo',JSON.stringify(playerInfo));
-    localStorage.setItem('totalPlayers',JSON.stringify(gameInfo.totalPlayers));
-    window.location = 'gamePlay.html';
-  } else {
-    alert('Please enter a name for each player');
-  }
-
-} else if (gameInfo.totalPlayers == 4) {
-  if(gameInfo.player1name && gameInfo.player2name && gameInfo.player3name && gameInfo.player4name) {
-    var player1 = new PlayerInfo('King Kandy', 'rgb(250, 161, 17)','graphics/KingKandy.png','cupCakesAndSundaes/cupCakeOrange.svg','cupCakesAndSundaes/sundeaOrange.svg',gameInfo.player1name);
-    var player2 = new PlayerInfo('Lord Licorice', 'rgb(202, 55, 55)','graphics/lordLicorice.png','cupCakesAndSundaes/cupCakeRed.svg','cupCakesAndSundaes/sundeaRed.svg',gameInfo.player2name);
-    var player3 = new PlayerInfo('Queen Frostine', 'rgb(102, 173, 229)','graphics/queenFrostine.png','cupCakesAndSundaes/cupCakeBlue.svg','cupCakesAndSundaes/sundeaBlue.svg',gameInfo.player3name);
-    var player4 = new PlayerInfo('Gloppy', 'rgb(121, 82, 51)','graphics/gloppy.png','cupCakesAndSundaes/cupCakeBrown.svg','cupCakesAndSundaes/sundeaBrown.svg',gameInfo.player4name);
-    playerInfo = [player1,player2,player3,player4];
-    localStorage.setItem('currentPlayerInfo',JSON.stringify(playerInfo));
-    localStorage.setItem('totalPlayers',JSON.stringify(gameInfo.totalPlayers));
-    window.location = 'gamePlay.html';
-  } else {
-    alert('Please enter a name for each player');
-  }
-
-} else {
-  alert('Please select a total number of players by tapping or clicking the numbers');
 }
 
-});
+function checkPlayerNames() {
+  gameInfo.player1name = $('#player1name').val();
+  gameInfo.player2name = $('#player2name').val();
+  gameInfo.player3name = $('#player3name').val();
+  gameInfo.player4name = $('#player4name').val();
+
+  if(gameInfo.totalPlayers == 3) {
+    if(gameInfo.player1name && gameInfo.player2name && gameInfo.player3name) {
+      var player1 = new PlayerInfo('King Kandy', 'rgb(250, 161, 17)','graphics/KingKandy.png','cupCakesAndSundaes/cupCakeOrange.svg','cupCakesAndSundaes/sundeaOrange.svg',gameInfo.player1name);
+      var player2 = new PlayerInfo('Lord Licorice', 'rgb(202, 55, 55)','graphics/lordLicorice.png','cupCakesAndSundaes/cupCakeRed.svg','cupCakesAndSundaes/sundeaRed.svg',gameInfo.player2name);
+      var player3 = new PlayerInfo('Queen Frostine', 'rgb(102, 173, 229)','graphics/queenFrostine.png','cupCakesAndSundaes/cupCakeBlue.svg','cupCakesAndSundaes/sundeaBlue.svg',gameInfo.player3name);
+      playerInfo = [player1,player2,player3];
+      localStorage.setItem('currentPlayerInfo',JSON.stringify(playerInfo));
+      localStorage.setItem('totalPlayers',JSON.stringify(gameInfo.totalPlayers));
+      window.location = 'gamePlay.html';
+    } else {
+      alert('Please enter a name for each player');
+    }
+
+  } else if (gameInfo.totalPlayers == 4) {
+    if(gameInfo.player1name && gameInfo.player2name && gameInfo.player3name && gameInfo.player4name) {
+      var player1 = new PlayerInfo('King Kandy', 'rgb(250, 161, 17)','graphics/KingKandy.png','cupCakesAndSundaes/cupCakeOrange.svg','cupCakesAndSundaes/sundeaOrange.svg',gameInfo.player1name);
+      var player2 = new PlayerInfo('Lord Licorice', 'rgb(202, 55, 55)','graphics/lordLicorice.png','cupCakesAndSundaes/cupCakeRed.svg','cupCakesAndSundaes/sundeaRed.svg',gameInfo.player2name);
+      var player3 = new PlayerInfo('Queen Frostine', 'rgb(102, 173, 229)','graphics/queenFrostine.png','cupCakesAndSundaes/cupCakeBlue.svg','cupCakesAndSundaes/sundeaBlue.svg',gameInfo.player3name);
+      var player4 = new PlayerInfo('Gloppy', 'rgb(121, 82, 51)','graphics/gloppy.png','cupCakesAndSundaes/cupCakeBrown.svg','cupCakesAndSundaes/sundeaBrown.svg',gameInfo.player4name);
+      playerInfo = [player1,player2,player3,player4];
+      localStorage.setItem('currentPlayerInfo',JSON.stringify(playerInfo));
+      localStorage.setItem('totalPlayers',JSON.stringify(gameInfo.totalPlayers));
+      window.location = 'gamePlay.html';
+    } else {
+      alert('Please enter a name for each player');
+    }
+  } else {
+    alert('Please select a total number of players by tapping or clicking the numbers');
+  }
+}
+
+
+$(function() {
+var hasTouch = 'ontouchstart' in window;
+
+if (hasTouch) {
+  $('#startNewGame').on('touchend',resetGame);
+
+  $('.playerNumber').on('touchend',checkNumberOfPlayers);
+
+  $('#submit').click('touchend',checkPlayerNames);
+
+} else {
+
+  $('#startNewGame').click(resetGame);
+
+  $('.playerNumber').click(checkNumberOfPlayers);
+
+  $('#submit').click(checkPlayerNames);
+
+}
+})
